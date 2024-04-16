7925b1ff98ea874f417ccd743270d2e30f3ca1e1
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-04-16 23:22:21 +0200

    feat(Marquee): Create Marquee component and  styles

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,19 @@
+import '../styles/index.css';
+
+export function Marquee() {
+    return (
+        <div className='marquee-container'>
+            <div className="marquee">
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+                <span className='marquee__text'>discover the range &nbsp;</span>
+            </div>
+        </div>
+    );
+}
