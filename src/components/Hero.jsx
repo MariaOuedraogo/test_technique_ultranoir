f171cee515fa7a5ede42f1ae304616f510a73e3d
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-04-18 19:37:19 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,14 @@ import { Slider } from './Slider';
 export function Hero(props) {
     return (
         <div className="hero">
-            <img src="../public/logo_dior.svg" alt=""  className='hero__img'/>
+            <img src="../public/logo_dior.svg" alt="" className='hero__img' />
             <h1 className='hero__title'>notre <br />savoir faire</h1>
             {props.children}
             <Slider>
 
             </Slider>
+
+
         </div>
     );
 }
