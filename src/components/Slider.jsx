e1dbcf10fc58c93496d331a46f327ef6275500d8
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-04-18 19:35:04 +0200

    feat(slider): Added desktop version + styling
    This commit pushes the final code for the slider component, including the addition of the desktop version utilizing GSAP.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,8 @@
 import '../styles/index.css';
 import { Button } from './Button';
 import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+
 
 
 const sliderComponent = [
@@ -8,7 +10,7 @@ const sliderComponent = [
 
         title: "discovery \n of the garden",
         subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem. ",
-        subtitleShort:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        subtitleShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.",
         detailsShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam.",
         img: "/src/assets/images/slider/garden.jpg"
@@ -17,9 +19,9 @@ const sliderComponent = [
 
     {
 
-        title: "extraction essences",
+        title: "extraction \n essences",
         subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem. ",
-        subtitleShort:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        subtitleShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.",
         detailsShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam.",
         img: "/src/assets/images/slider/essences.jpg"
@@ -28,9 +30,9 @@ const sliderComponent = [
 
     {
 
-        title: "the symbiosis lorem ipsum",
+        title: "the symbiosis \n lorem ipsum",
         subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem. ",
-        subtitleShort:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        subtitleShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.",
         detailsShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam.",
         img: "/src/assets/images/slider/symbiosis.jpg"
@@ -40,7 +42,7 @@ const sliderComponent = [
 
         title: "from flower \n to bottle",
         subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem. ",
-        subtitleShort:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        subtitleShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.",
         detailsShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam.",
         img: "/src/assets/images/slider/flower_to_bottle.jpg"
@@ -49,7 +51,7 @@ const sliderComponent = [
     {
         title: "miss dior \n scent",
         subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem. ",
-        subtitleShort:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        subtitleShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.",
         detailsShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam.",
         img: "/src/assets/images/slider/miss_dior.jpg"
@@ -68,54 +70,192 @@ export function Slider() {
         let startX, startScrollLeft;
 
         const dragStart = (e) => {
-            startX = e.touches[0].clientX;
+            if (e.type === 'touchstart') {
+                startX = e.touches[0].clientX;
+            } else {
+                startX = e.clientX;
+            }
             startScrollLeft = sliding.scrollLeft;
         }
 
         const dragging = (e) => {
-            const currentX = e.touches[0].clientX;
-            const diffX = startX - currentX;
-            sliding.scrollLeft = startScrollLeft + diffX;
+            e.preventDefault(); // Pour empêcher le défilement par défaut pendant le drag and drop
+            if (startX !== undefined) {
+                const currentX = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
+                const diffX = startX - currentX;
+                sliding.scrollLeft = startScrollLeft + diffX;
+            }
+        }
+
+        const dragEnd = () => {
+            startX = undefined;
+            // Peut-être ajouter des opérations supplémentaires lors du relâchement du glisser-déposer
         }
 
         sliding.addEventListener('touchstart', dragStart);
         sliding.addEventListener('touchmove', dragging);
+        sliding.addEventListener('touchend', dragEnd);
+        sliding.addEventListener('mousedown', dragStart);
+        sliding.addEventListener('mousemove', dragging);
+        sliding.addEventListener('mouseup', dragEnd);
+        sliding.addEventListener('mouseleave', dragEnd);
 
         return () => {
             sliding.removeEventListener('touchstart', dragStart);
             sliding.removeEventListener('touchmove', dragging);
+            sliding.removeEventListener('touchend', dragEnd);
+            sliding.removeEventListener('mousedown', dragStart);
+            sliding.removeEventListener('mousemove', dragging);
+            sliding.removeEventListener('mouseup', dragEnd);
+            sliding.removeEventListener('mouseleave', dragEnd);
         };
     }, []);
+
+
+
+    const windowHeight = window.innerHeight;
+    const imgHeight = 0.95 * windowHeight;
+    const titleHeight = 117;
+    const numHeight = 30;
+
+    let currentIndex = 0; // Ajoutez une variable pour suivre l'index actuel du slider
+
+    const animateUp = () => {
+        // Déplacez vers le haut seulement si ce n'est pas la dernière image
+        if (currentIndex < sliderComponent.length - 1) {
+            gsap.to('.desk-slide__image-content', { y: `-=${imgHeight}` });
+            gsap.to('.desk-slide__title-content', { y: `-=${titleHeight}` });
+            gsap.to('.desk-slide__num-content', { y: `-=${numHeight}` });
+            currentIndex++;
+        } else {
+            // Si c'est la dernière image, retournez à la première
+            gsap.to('.desk-slide__image-content', { y: 0 });
+            gsap.to('.desk-slide__title-content', { y: 0 });
+            gsap.to('.desk-slide__num-content', { y: 0 });
+            currentIndex = 0;
+        }
+    };
+
+    const animateDown = () => {
+        // Déplacez vers le bas seulement si ce n'est pas la première image
+        if (currentIndex > 0) {
+            gsap.to('.desk-slide__image-content', { y: `+=${imgHeight}` });
+            gsap.to('.desk-slide__title-content', { y: `+=${titleHeight}` });
+            gsap.to('.desk-slide__num-content', { y: `+=${numHeight}` });
+            currentIndex--;
+        } else {
+            // Si c'est la première image, allez à la dernière
+            const lastIndex = sliderComponent.length - 1;
+            const imgDistanceToMove = -1 * lastIndex * imgHeight;
+            const titleDistanceToMove = -1 * lastIndex * titleHeight;
+            const numDistanceToMove = -1 * lastIndex * numHeight;
+            gsap.to('.desk-slide__image-content', { y: imgDistanceToMove });
+            gsap.to('.desk-slide__title-content', { y: titleDistanceToMove });
+            gsap.to('.desk-slide__num-content', { y: numDistanceToMove });
+            currentIndex = lastIndex;
+
+        }
+    };
+
     return (
-        <div className="slides-container">
-            {sliderComponent.map((slide, index) => (
-                <div key={index} className="slides">
-                    <div className="slides__image-container">
-                        <img src={slide.img} alt="" className="slides__image" />
+        <React.Fragment>
+
+            <div className="slides-container slides-container--mobile">
+                {sliderComponent.map((slide, index) => (
+                    <div key={index} className="slide">
+                        <div className="slide__image-container">
+                            <img src={slide.img} alt="" className="slide__image" />
+                        </div>
+                        <div className="slide__text-container">
+                            <h2 className='slide__title'>
+                                {slide.title.split('\n').map((line, i) => (
+                                    <React.Fragment key={i}>
+                                        {line}
+                                        <br />
+                                    </React.Fragment>
+                                ))}
+                            </h2>
+                            <h3 className='slide__subtitle'>{slide.subtitleShort}</h3>
+                            <p className='slide__text'>{slide.detailsShort}</p>
+                            <Button className='slide__button'>
+                                <img src="/src/assets/images/add_to_cart.svg" alt="" className='slide__button-img' />
+                                <p className='slide__button-text'>
+                                    Order
+                                </p>
+                            </Button>
+                            <p className='slide__num'>
+                                {index + 1 <= 5 ? `0${index + 1}` : index++} / 05
+                            </p>
+                        </div>
                     </div>
-                    <div className="slides__text-container">
-                        <h1 className='slides__title'>
-                            {slide.title.split('\n').map((line, i) => (
-                                <React.Fragment key={i}>
-                                    {line}
-                                    <br />
-                                </React.Fragment>
-                            ))}
-                        </h1>
-                        <h2 className='slides__subtitle'>{slide.subtitleShort}</h2>
-                        <p className='slides__text'>{slide.detailsShort}</p>
-                        <Button className='slides__button'>
-                            <img src="/src/assets/images/add_to_cart.svg" alt="" className='slides_button-img' />
-                            <p className='slides__button-text'>
+                ))}
+            </div>
+
+
+
+            <div className="desk-slides-container desk-slides-container--show ">
+
+                <div className="desk-slide">
+
+                    <div className="desk-slide__text-container">
+                        <img src="src/assets/images/slider/flower_slider1.png" alt="" className='desk-slide__text-img desk-slide__text-img--left' />
+
+                        <div className="desk-slide__num-container">
+                            <div className="desk-slide__num-content">
+                                {sliderComponent.map((slide, index) => (
+                                    <p className="desk-slide__num"  >
+                                        {index + 1 <= 5 ? `0${index + 1}` : index++} / 05
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="desk-slide__title-container">
+                            <div className="desk-slide__title-content">
+                                {sliderComponent.map((slide, index) => (
+                                    <h2 className="desk-slide__title">{slide.title.split('\n').map((line, i) => (
+                                        <React.Fragment key={i}>
+                                            {line}
+                                            <br />
+                                        </React.Fragment>
+                                    ))}</h2>
+                                ))}
+                            </div>
+                        </div>
+
+                        <h3 className="desk-slide__subtitle"> Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit lorem.  </h3>
+                        <p className="desk-slide__text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget velit a elit finibus lacinia id at diam. Proin placerat dignissim risus ac finibus. Sed fermentum, sem et dignissim venenatis, felis massa volutpat eros.  </p>
+
+                        <Button className='desk-slide__button'>
+                            <img src="/src/assets/images/add_to_cart.svg" alt="" className='slide__button-img' />
+                            <p className='slide__button-text'>
                                 Order
                             </p>
                         </Button>
-                        <p className='slides__num'>
-                            {index + 1 <= 5 ? `0${index + 1}` : index++} / 05
-                        </p>
+                        <div className="desk-slide__bottom-container">
+                            <div className="desk-slide__controls">
+                                <img src="src/assets/images/slider/arrow_left.png" alt="" className='desk-slide__arrow desk-side__arrow--left' onClick={animateDown} />
+                                <img src="src/assets/images/slider/arrow_right.png" alt="" className='desk-slide__arrow desk-side__arrow--right' onClick={animateUp} />
+                            </div>
+                            <img src="src/assets/images/slider/flower_slider2.png" alt="" className='desk-slide__text-img desk-slide__text-img--right' />
+                        </div>
                     </div>
+
+
+                    <div className="desk-slide__image-container">
+                        <div className="desk-slide__image-content">
+                            {sliderComponent.map((slide, index) => (
+                                <img className='desk-slide__image' src={slide.img} alt="" />
+                            ))}
+                        </div>
+                    </div>
+
+
                 </div>
-            ))}
-        </div>
+
+            </div>
+
+        </React.Fragment>
+
     );
 }
