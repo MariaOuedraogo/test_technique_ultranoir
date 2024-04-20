668a66c418749388b071c653c8e8d6ccc5b5d3c1
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-04-21 00:18:10 +0200

    feat(hero): animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,21 @@
 import "../styles/index.css";
 import { Slider } from "./Slider";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+
 
 // section with the logo, and slider
 export function Hero(props) {
+
+    useEffect(() => {
+        // smooth animation for the hero section
+        gsap.fromTo(".hero__img", {opacity: 0 }, { opacity: 1, duration: 1, delay: .5 })
+        gsap.fromTo(".desk-slide__image", { scale: .99, opacity: 0 }, { scale: 1, opacity: 1, duration: 2, delay: 1 })
+        gsap.fromTo(".desk-slide__title, .desk-slide__subtitle, .desk-slide__text, .desk-slide__button, .desk-slide__num", { opacity: 0 }, { opacity: 1, duration: 2, delay: 1, ease: "power2.out" });
+        gsap.fromTo(".desk-slide__text-img--left, .desk-slide__text-img--right", { scale: 0,  }, { scale: 1, duration: 2, delay: 1.5 })
+        gsap.fromTo(".desk-slide__controls", { opacity: 0 }, { opacity: 1, duration: 2, delay: 2.5, ease: "power2.out" });
+        
+    },[]);
 
     return (
         <div className="hero">
