a918c5898dc0c1e183845a3bc31b2ddc7641bc30
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-04-22 03:51:21 +0200

    feat(discover): add listing component

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,6 +1,8 @@
 import "../styles/index.css";
 import React from "react";
 import { Marquee } from "./Marquee";
+import { Listing } from "./Listing";
+
 
 // section with the marquee and the product listing
 export function Discover(props) {
@@ -9,6 +11,8 @@ export function Discover(props) {
 
                 {props.children}
                 <Marquee></Marquee>
+                <Listing></Listing>
+
             </div>
         
     );
